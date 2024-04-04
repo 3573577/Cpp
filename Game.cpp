482fe43f3c53248ba3a67028e5cd29bb2fdec3ae
3573577 <165747558+3573577@users.noreply.github.com>
2024-04-04 17:38:30 +0530

    Create Game.cpp

---
+++ Game.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<windows>
+#include<conio.h>
+using namespace std;
+const int height = 20;
+const int width = 17;
+int x,y,score,sntailx[100],sntaily[100],sntaillen,fruity,fruitx;
+enum direction ={stop=0,left,right,up,down};
+direction sdir;
+bool gameover;
+void init(){
+  gameover=false;
+x=height/2;
+y=width/2;
+direction=stop;
+fruity=rand%;
+fruitx=rand%;
+score=0;
+}
+void draw (){
+  system ("cls)
+for(int i=0;i<height;i++){
+cout<<"#";
+cout<<endl;
+for(int i=0;i<width+2;i++){
+for(int j=1;j<width;j++){
+
+}
+}
+}
